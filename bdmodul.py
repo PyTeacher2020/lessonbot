807284df262ca2f50319a12b5d525517b7817246
PyTeacher2020 <veliade.saliieva@nure.ua>
2020-12-02 15:31:51 +0200

    аааа

--- bdmodul.py
+++ bdmodul.py
@@ -1,9 +1,6 @@
 import requests
 
 def vk():
-    proxies = {
-      'http': 'http://198.16.66.197:8080',
-      'https': 'http://198.16.66.197:8080',
-    }
 
-    return requests.get('http://vk.com/', proxies=proxies)
+
+    return requests.get('http://vk.com/')
